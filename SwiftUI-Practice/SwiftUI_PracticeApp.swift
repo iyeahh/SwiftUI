66144c3688b74b614387975e0da3a34ad2e4a818
iyeahh <yboravender@gmail.com>
2024-09-02 14:20:33 +0900

    Initial Commit

---
+++ SwiftUI-Practice/SwiftUI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PracticeApp.swift
+//  SwiftUI-Practice
+//
+//  Created by Bora Yang on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
